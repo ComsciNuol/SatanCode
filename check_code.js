bafb929f478164d89146aa5ba9e7f72fd70802b9
ChanLor <44386785+ChanLor@users.noreply.github.com>
2018-12-17 21:35:36 +0700

    Add files via upload

---
+++ check_code.js
@@ -0,0 +1,12 @@
+function check() {
+	var code = document.getElementById('txt_code').value;
+	var arr = new Array("3CW2-2018");
+	if (code == arr[0]) {
+		window.location.assign("https://drive.google.com/open?id=14_rUX1by9BVHcpzZcf043qxrGTYnQfvF");
+		// console.log("yesssss");
+	}else{
+		alert("plase enter your code")
+		// console.log("nooooo")
+		// console.log(code)
+	}
+}
